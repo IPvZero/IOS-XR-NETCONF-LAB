b168ca5140e5deb099473047caffd6a18f0e0df9
IPvZero <46572101+IPvZero@users.noreply.github.com>
2021-01-16 00:17:26 +0000

    Create get-bgp.py

---
+++ get-bgp.py
@@ -0,0 +1,30 @@
+"""
+AUTHOR: IPvZero
+Date: 15 January 2021
+
+This script retrieves BGP configuration information
+"""
+
+from nornir_scrapli.tasks import netconf_get_config
+from nornir_utils.plugins.functions import print_result
+from nornir import InitNornir
+
+nr = InitNornir(config_file="config.yaml")
+
+acl_filter = """
+<bgp xmlns="http://cisco.com/ns/yang/Cisco-IOS-XR-ipv4-bgp-cfg">
+</bgp> """
+
+
+def get_yang(task):
+    """ Pull BGP Configuration """
+    task.run(
+        task=netconf_get_config,
+        source="running",
+        filter_type="subtree",
+        filters=acl_filter,
+    )
+
+
+result = nr.run(task=get_yang)
+print_result(result)
